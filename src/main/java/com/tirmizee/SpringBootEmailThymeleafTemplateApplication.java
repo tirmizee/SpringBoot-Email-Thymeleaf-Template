7d697da08f06edf02cb00ef1b79178fe09cb222a
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-09-27 15:24:06 +0700

    test load bean

--- src/main/java/com/tirmizee/SpringBootEmailThymeleafTemplateApplication.java
+++ src/main/java/com/tirmizee/SpringBootEmailThymeleafTemplateApplication.java
@@ -1,13 +1,26 @@
 package com.tirmizee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @SpringBootApplication
-public class SpringBootEmailThymeleafTemplateApplication {
+public class SpringBootEmailThymeleafTemplateApplication implements CommandLineRunner {
 
+	@Autowired
+	ApplicationContext applicationContext;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEmailThymeleafTemplateApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		SpringTemplateEngine templateEngine = applicationContext.getBean(SpringTemplateEngine.class);
+		System.out.println(templateEngine != null);
 	}
 
 }
